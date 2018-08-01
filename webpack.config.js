3358c8c59a3c18ae35dd9ae4dd7cbd1909b6f03e
Anxin <anxinyang114@gmail.com>
2018-08-01 15:39:11 -0700

    cc

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    node: {
+        fs: 'empty',
+        child_process: 'empty'
+    },
+    module: {
+        rules: [
+            {
+                test: /js?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx','.css']
+    },
+    output: {
+        path: __dirname + '/',
+        filename: '[name].js'
+    }
+
+};
