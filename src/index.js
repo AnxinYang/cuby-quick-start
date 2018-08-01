242dac60dfce7eccc401920895f3b3925772d294
Anxin <anxinyang114@gmail.com>
2018-08-01 16:54:37 -0700

    cc

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
  * Created by Anxin Yang on 3/28/2018.
  */
 import CubY from './cuby/CubY';
+window.root = new CubY.createElement('div', 'cuby_root', document.getElementById('app'));
+assetLoaded('main');
