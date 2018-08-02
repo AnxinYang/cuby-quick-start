ea2365d90bcfdbed8a830f10978758250a19e115
Anxin <anxinyang114@gmail.com>
2018-08-01 18:44:15 -0700

    cc

--- src/index.js
+++ src/index.js
@@ -16,12 +16,14 @@ let mainContentSection = CubY.createElement('div', 'mainContentSection')
         position: 'relative',
         minWidth: '300px',
         width:'66.66%',
-        flexGrow: 1
+        flexGrow: 1,
+        transition:'0.2s'
     });
 let secondContentSection = CubY.createElement('div', 'secondContentSection').style({
     position: 'relative',
     minWidth: '100px',
-    flexGrow: 1
+    flexGrow: 1,
+    transition:'0.2s'
 });
 
 let mainCard = CubY.createCard('mainCard');
@@ -33,9 +35,27 @@ root.appendElement(secondContentSection);
 
 mainContentSection.appendElement(mainCard);
 
+// Setup connection
+
+CubY.connect('viewportSize').to(function (size) {
+    console.log(size.width);
+    if(size.width<=800) {
+        mainContentSection.style({
+            width: '100%'
+        })
+    }else{
+        mainContentSection.style({
+            width: '66.66%'
+        })
+    }
+});
 
 //setup basic listener
 window.addEventListener('resize', function () {
-
+    let viewportSize = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+    CubY.storeValue('viewportSize', viewportSize);
 });
 assetLoaded('main');
