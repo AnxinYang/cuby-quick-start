bb6cf6c7dd445e0875410e9fdfc88828a3fc6f7b
Anxin <anxinyang114@gmail.com>
2018-08-02 16:42:30 -0700

    cc

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,7 +1,17 @@
 import CubY from "../cuby/CubY";
 
-const TODO_LIST_STYLE = {
-
+const LIST_ITEM_CONTAINER_STYLE = {
+    width: '95%',
+    color: 'rgba(0,0,0,0.87)',
+    cursor: 'pointer',
+    position: 'relative',
+    transition: '0.2s',
+    textAlign: 'left',
+    padding: '1em 0.5em',
+    borderBottom: '1px solid rgba(243,243,243,.85)',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden'
 };
 
 let todoList = {
@@ -14,7 +24,15 @@ let todoList = {
         })
     },
     createTodoListItem: function (item) {
-        let component = CubY.createElement('div', item.id);
+        let component = CubY.createElement('div', item.id).content(item.title)
+            .style(LIST_ITEM_CONTAINER_STYLE)
+            .on('mouseenter',function () {
+                this.style('background', '#eee')
+            })
+            .on('mouseleave',function () {
+                this.style('background', '')
+            });
+
         return component;
     }
 };
